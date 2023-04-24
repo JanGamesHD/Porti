f92952e551e3e4b0c2c2a1069a33943078c9094f
JanGamesHD <64363088+JanGamesHD@users.noreply.github.com>
2023-04-24 20:52:49 +0200

    Add files via upload

---
+++ Store_loader.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Tauser Store loader
+echo made by JanGames and FBW81C
+echo Current CD: %cd%
+cd \
+echo Current CD: %cd%
+echo System Drive: %systemdrive%
+set oldsystemdrive=%systemdrive%
+set systemdrive=%cd:\=%
+set systemdrive=%systemdrive%\Porti\Store
+md %systemdrive%
+echo Changed System Drive from %oldsystemdrive% to %systemdrive%
+echo Loading Tauser Store...
+set useporti=1
+call %cd%\PORTI\Store\Store.bat
