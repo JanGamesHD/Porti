98c7e873596b58ff6668ae5fd0cdab355334fcb8
JanGamesHD <64363088+JanGamesHD@users.noreply.github.com>
2023-06-06 18:31:48 +0200

    Update Store_loader.bat

--- Store_loader.bat
+++ Store_loader.bat
@@ -2,14 +2,25 @@
 echo Tauser Store loader
 echo made by JanGames and FBW81C
 echo Current CD: %cd%
-cd \
+rem cd \
 echo Current CD: %cd%
 echo System Drive: %systemdrive%
-set oldsystemdrive=%systemdrive%
-set systemdrive=%cd:\=%
-set systemdrive=%systemdrive%\Porti\Store
+rem set oldsystemdrive=%systemdrive%
+rem set systemdrive=%cd:\=%
+set systemdrive=Porti\Store
 md %systemdrive%
 echo Changed System Drive from %oldsystemdrive% to %systemdrive%
 echo Loading Tauser Store...
 set useporti=1
-call %cd%\PORTI\Store.bat
+if not exist PORTI\Store.bat goto 2
+call PORTI\Store.bat
+set error=%errorlevel%
+goto aftercall
+exit
+:2
+call Store.bat
+set error=%errorlevel%
+:aftercall
+echo Tause Store exited with error code %error%.
+pause
+exit
